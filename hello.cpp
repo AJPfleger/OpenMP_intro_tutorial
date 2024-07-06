97bd05816a8d3d15620f7209677c7a4d93baf2dc
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-07-06 20:25:12 +0900

    basis pi

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,11 @@
 
 int main () {
 
+  // omp_set_num_threads(4);
+  // #pragma omp parallel
+  // or
+  // #pragma omp parallel num_threads(4)
+  // or for maximum
   #pragma omp parallel
   {
     int threadId = omp_get_thread_num();
