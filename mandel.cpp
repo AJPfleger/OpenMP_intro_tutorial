6184139c32b82f2b9f5373d1532fab21e58aa435
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-07-06 23:11:20 +0900

    cpp mandel

---
+++ mandel.cpp
@@ -0,0 +1,78 @@
+/*
+**  PROGRAM: Mandelbrot area
+**
+**  PURPOSE: Program to compute the area of a  Mandelbrot set.
+**           Correct answer should be around 1.510659.
+**           WARNING: this program may contain errors
+**
+**  USAGE:   Program runs without input ... just run the executable
+**            
+**  HISTORY: Written:  (Mark Bull, August 2011).
+**           Changed "comples" to "d_comples" to avoid collsion with 
+**           math.h complex type (Tim Mattson, September 2011)
+** cpp adaption: AJPfleger
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+struct d_complex{
+    double r;
+    double i;
+};
+
+struct d_complex c;
+int numoutside = 0;
+
+// Does the iteration z=z*z+c, until |z| > 2 when point is known to be outside set
+// If loop count reaches MAXITER, point is considered to be inside the set
+void testpoint(const int max_iter = 1000){
+  struct d_complex z;
+  int iter;
+  double temp;
+
+  z = c;
+  for (iter=0; iter < max_iter; iter++){
+    temp = (z.r * z.r) - (z.i * z.i) + c.r;
+    z.i = z.r * z.i * 2 + c.i;
+    z.r = temp;
+    if ((z.r * z.r + z.i * z.i) > 4.) {
+      numoutside++;
+      break;
+    }
+  }
+
+}
+
+int main() {
+//  const
+  double eps = 1.e-5;
+  const int n_points = 1000;
+  const int max_iter = 1000;
+
+  //   Loop over grid of points in the complex plane which contains the Mandelbrot set,
+  //   testing each point to see whether it is inside or outside the set.
+
+  #pragma omp parallel for default(shared) private(c, eps)
+  for (int i = 0; i < n_points; i++) {
+    for (int j = 0; j < n_points; j++) {
+      c.r = -2. + 2.5 * static_cast<double>(i) / static_cast<double>(n_points) + eps;
+      c.i = 1.125 * static_cast<double>(j) / static_cast<double>(n_points) + eps;
+      testpoint(max_iter);
+    }
+  }
+
+  // Calculate area of set and error estimate and output the results
+  const double area = 2.0 * 2.5 * 1.125 *
+      static_cast<double>(n_points*n_points-numoutside) / static_cast<double>(n_points * n_points);
+  double error = area / (double)n_points;
+
+  printf("Area of Mandelbrot set = %12.8f +/- %12.8f\n", area, error);
+  printf("Correct answer should be around 1.510659\n");
+
+}
+
+
+
--- pi.cpp
+++ pi.cpp
@@ -22,7 +22,7 @@ cpp-port: AJPfleger
 
 #include <omp.h>
 
-double calcPi(const long num_steps, const std::uint32_t num_threads) {
+double calcPiVector(const long num_steps, const std::uint32_t num_threads) {
   const double step = 1.0 / static_cast<double>(num_steps);
   std::vector<double> sum(num_threads, 0.);
 
@@ -42,14 +42,139 @@ double calcPi(const long num_steps, const std::uint32_t num_threads) {
   return pi;
 }
 
+double calcPiCritical(const long num_steps, const std::uint32_t num_threads) {
+  const double step = 1.0 / static_cast<double>(num_steps);
+  double pi = 0.;
+
+  #pragma omp parallel num_threads(num_threads)
+  {
+    const int id = omp_get_thread_num();
+    double sub_sum = 0.;
+    for (long i = num_steps - id; i > 0; i -= num_threads) {
+      double x = (i - 0.5) * step;
+      sub_sum += 4. / (1. + x * x);
+    }
+
+    #pragma omp critical
+    pi += step * sub_sum;
+  }
+
+  return pi;
+}
+
+double calcPiAtomic(const long num_steps, const std::uint32_t num_threads) {
+  const double step = 1.0 / static_cast<double>(num_steps);
+  double pi = 0.;
+
+  #pragma omp parallel num_threads(num_threads)
+  {
+    const int id = omp_get_thread_num();
+    double sub_sum = 0.;
+    for (long i = num_steps - id; i > 0; i -= num_threads) {
+      double x = (i - 0.5) * step;
+      sub_sum += 4. / (1. + x * x);
+    }
+
+    double sub_pi = step * sub_sum;
+
+    #pragma omp atomic
+    pi += sub_pi;
+  }
+
+  return pi;
+}
+
+double calcPiFor(const long num_steps, const std::uint32_t num_threads) {
+  const double step = 1.0 / static_cast<double>(num_steps);
+  double pi = 0.;
+
+  #pragma omp parallel num_threads(num_threads)
+  {
+    double sub_sum = 0.;
+    #pragma omp for
+    for (long i = num_steps; i > 0; i--) {
+      double x = (i - 0.5) * step;
+      sub_sum += 4. / (1. + x * x);
+    }
+
+    double sub_pi = step * sub_sum;
+
+    #pragma omp atomic
+    pi += sub_pi;
+  }
+
+  return pi;
+}
+
+double calcPiReduction(const long num_steps, const std::uint32_t num_threads) {
+  const double step = 1.0 / static_cast<double>(num_steps);
+  double sum = 0.;
+
+  #pragma omp parallel num_threads(num_threads)
+  {
+    #pragma omp for reduction(+:sum)
+    for (long i = num_steps; i > 0; i--) {
+      double x = (i - 0.5) * step;
+      sum += 4. / (1. + x * x);
+    }
+  }
+
+  double pi = step * sum;
+
+  return pi;
+}
+
 int main () {
   const long num_steps = 100000000;
   const std::uint32_t min_threads = 1;
   const std::uint32_t max_threads = 8;
 
+  std::cout << "Running " << num_steps << " steps:" << std::endl;
+
+  std::cout << "\ncalcPiVector:" << std::endl;
   for (auto num_threads = min_threads; num_threads <= max_threads; num_threads++) {
     double start_time = omp_get_wtime();
-    double pi = calcPi(num_steps, num_threads);
+    double pi = calcPiVector(num_steps, num_threads);
+    double run_time = omp_get_wtime() - start_time;
+
+    std::cout << num_threads << " threads: pi = " << pi << " in " << run_time
+              << " seconds." << std::endl;
+  }
+
+  std::cout << "\ncalcPiCritical:" << std::endl;
+  for (auto num_threads = min_threads; num_threads <= max_threads; num_threads++) {
+    double start_time = omp_get_wtime();
+    double pi = calcPiCritical(num_steps, num_threads);
+    double run_time = omp_get_wtime() - start_time;
+
+    std::cout << num_threads << " threads: pi = " << pi << " in " << run_time
+              << " seconds." << std::endl;
+  }
+
+  std::cout << "\ncalcPiAtomic:" << std::endl;
+  for (auto num_threads = min_threads; num_threads <= max_threads; num_threads++) {
+    double start_time = omp_get_wtime();
+    double pi = calcPiAtomic(num_steps, num_threads);
+    double run_time = omp_get_wtime() - start_time;
+
+    std::cout << num_threads << " threads: pi = " << pi << " in " << run_time
+              << " seconds." << std::endl;
+  }
+
+  std::cout << "\ncalcPiFor:" << std::endl;
+  for (auto num_threads = min_threads; num_threads <= max_threads; num_threads++) {
+    double start_time = omp_get_wtime();
+    double pi = calcPiFor(num_steps, num_threads);
+    double run_time = omp_get_wtime() - start_time;
+
+    std::cout << num_threads << " threads: pi = " << pi << " in " << run_time
+              << " seconds." << std::endl;
+  }
+
+  std::cout << "\ncalcPiReduction:" << std::endl;
+  for (auto num_threads = min_threads; num_threads <= max_threads; num_threads++) {
+    double start_time = omp_get_wtime();
+    double pi = calcPiReduction(num_steps, num_threads);
     double run_time = omp_get_wtime() - start_time;
 
     std::cout << num_threads << " threads: pi = " << pi << " in " << run_time
