3e405050b2ccd2f77df75ec0c5e9f196fff23ae5
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-07-06 21:46:03 +0900

    improve vector caching

--- pi.cpp
+++ pi.cpp
@@ -15,32 +15,46 @@ History: Written by Tim Mattson, 11/99.
 cpp-port: AJPfleger
 
 */
-#include <stdio.h>
-#include <omp.h>
+
+#include <iostream>
 #include <numeric>
 #include <vector>
 
-int main () {
-  const long num_steps = 100000000;
-  constexpr double step = 1.0 / static_cast<double>(num_steps);
-  const std::uint32_t num_threads = 8;
+#include <omp.h>
 
+double calcPi(const long num_steps, const std::uint32_t num_threads) {
+  const double step = 1.0 / static_cast<double>(num_steps);
   std::vector<double> sum(num_threads, 0.);
-
-  const double start_time = omp_get_wtime();
 
   #pragma omp parallel num_threads(num_threads)
   {
     const int id = omp_get_thread_num();
+    double sub_sum = 0.;
     for (long i = num_steps - id; i > 0; i -= num_threads) {
       double x = (i - 0.5) * step;
-      sum[id] += 4.0 / (1.0 + x * x);
+      sub_sum += 4. / (1. + x * x);
     }
+    sum[id] = sub_sum;
   }
 
-  const double pi = step * std::accumulate(sum.begin(), sum.end(), 0.);
+  double pi = step * std::accumulate(sum.begin(), sum.end(), 0.);
 
-  const double run_time = omp_get_wtime() - start_time;
+  return pi;
+}
 
-  printf("\n pi with %ld steps is %lf in %lf seconds\n ", num_steps, pi, run_time);
+int main () {
+  const long num_steps = 100000000;
+  const std::uint32_t min_threads = 1;
+  const std::uint32_t max_threads = 8;
+
+  for (auto num_threads = min_threads; num_threads <= max_threads; num_threads++) {
+    double start_time = omp_get_wtime();
+    double pi = calcPi(num_steps, num_threads);
+    double run_time = omp_get_wtime() - start_time;
+
+    std::cout << num_threads << " threads: pi = " << pi << " in " << run_time
+              << " seconds." << std::endl;
+  }
+
+  return 0;
 }
